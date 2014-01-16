5cdc9e799348e4c7d19f26513512f4c935411e84
kevin <kevin@KevDeb.KevDeb>
2014-01-16 11:21:13 -0600

    fixed google search library

--- discovery/googlesearch.py
+++ discovery/googlesearch.py
@@ -14,14 +14,15 @@ class search_google:
 		self.server_api="www.googleapis.com"
 		self.hostname="www.google.com"
 		self.userAgent="(Mozilla/5.0 (Windows; U; Windows NT 6.0;en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6"
-		self.quantity="100"
+		self.quantity="10"
 		self.limit=limit
 		self.counter=start
+		self.delay = 5
 		self.api_key="AIzaSyBuBomy0n51Gb4836isK2Mp65UZI_DrrwQ"
-		
+
 	def do_search(self):
 		h = httplib.HTTP(self.server)
-		h.putrequest('GET', "/search?num="+self.quantity+"&start=" + str(self.counter) + "&hl=en&meta=&q=%40\"" + self.word + "\"")
+		h.putrequest('GET', "/search?complete=0&num=100&start=" + str(self.counter) + "&hl=en&meta=&q=%40\"" + self.word + "\"")
 		h.putheader('Host', self.hostname)
 		h.putheader('User-agent', self.userAgent)	
 		h.endheaders()
@@ -43,7 +44,7 @@ class search_google:
 
 	def do_search_files(self,files):
 		h = httplib.HTTP(self.server)
-		h.putrequest('GET', "/search?num="+self.quantity+"&start=" + str(self.counter) + "&hl=en&meta=&q=filetype:"+files+"%20site:" + self.word)
+		h.putrequest('GET', "/search?complete=0&num=100&start=" + str(self.counter) + "&hl=en&meta=&q=filetype:"+files+"%20site:" + self.word)
 		h.putheader('Host', self.hostname)
 		h.putheader('User-agent', self.userAgent)	
 		h.endheaders()
@@ -53,14 +54,14 @@ class search_google:
 
 	def do_search_profiles(self):
 		h = httplib.HTTP(self.server)
-		h.putrequest('GET', '/search?num='+ self.quantity + '&start=' + str(self.counter) + '&hl=en&meta=&q=site:www.google.com%20intitle:"Google%20Profile"%20"Companies%20I%27ve%20worked%20for"%20"at%20' + self.word + '"')
+		h.putrequest('GET', '/search?complete=0&num='+ self.quantity + '&start=' + str(self.counter) + '&hl=en&meta=&q=site:www.google.com%20intitle:"Google%20Profile"%20"Companies%20I%27ve%20worked%20for"%20"at%20' + self.word + '"')
 		h.putheader('Host', self.hostname)
 		h.putheader('User-agent', self.userAgent)	
 		h.endheaders()
 		returncode, returnmsg, headers = h.getreply()
 		self.results = h.getfile().read()
 		self.totalresults+= self.results
-				
+
 	def check_next(self):
 		renext = re.compile('>  Next  <')
 		nextres=renext.findall(self.results)	
@@ -69,19 +70,19 @@ class search_google:
 		else:
 			nexty="0"
 		return nexty
-		
+
 	def get_emails(self):
 		rawres=myparser.parser(self.totalresults,self.word)
 		return rawres.emails()
-	
+
 	def get_hostnames(self):
 		rawres=myparser.parser(self.totalresults,self.word)
 		return rawres.hostnames()
-	
+
 	def get_files(self):
 		rawres=myparser.parser(self.totalresults,self.word)
 		return rawres.fileurls(self.files)
-	
+
 	def get_profiles(self):
 		rawres=myparser.parser(self.totalresults,self.word)
 		return rawres.profiles()
@@ -90,7 +91,7 @@ class search_google:
 		while self.counter <= self.limit and self.counter <= 1000:
 			self.do_search()
 			#more = self.check_next()
-			time.sleep(1)
+			time.sleep(self.delay)
 			print "\tSearching "+ str(self.counter) + " results..."
 			self.counter+=100
 	def process_api(self):
@@ -101,7 +102,7 @@ class search_google:
 	def process_files(self,files):
 		while self.counter <= self.limit:
 			self.do_search_files(files)
-			time.sleep(1)
+			time.sleep(self.delay)
 			self.counter+=100
 			print "\tSearching "+ str(self.counter) + " results..."
 
@@ -111,4 +112,4 @@ class search_google:
 			time.sleep(0.3)
 			self.counter+=100
 			print "\tSearching "+ str(self.counter) + " results..."
-	
+
