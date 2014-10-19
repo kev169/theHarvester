1bbf4fb09f65c234623830778fe6dc4e8a7303fe
kevin <kevin@KevDebLen.mobile>
2014-10-18 22:17:36 -0400

    Added comboview population

--- GUI.py
+++ GUI.py
@@ -2,18 +2,37 @@ __author__ = 'Kevin Haubris'
 
 from gi.repository import Gtk as gtk
 
+
 class WindowHandler:
+    data = ["google", "bing", "other"]
 
     def run(self):
         self.builder = gtk.Builder()
         self.builder.add_from_file("Design/Layout/harvester-gui.glade")
         self.builder.connect_signals(self)
         self.window = self.builder.get_object("main")
+        self.cell = gtk.CellRendererText()
+        name_store = gtk.ListStore(int,str)
+        for count, item in enumerate(self.data):
+            name_store.append((count, item))
+        comboview = self.builder.get_object("combobox1")
+        comboview.set_model(name_store)
+        comboview.pack_start(self.cell, True)
+        comboview.add_attribute(self.cell, 'text',1)
+        comboview.set_active(0)
         self.window.show_all()
         gtk.main()
 
     def onDeleteWindow(self, *args):
         gtk.main_quit(*args)
+
+    """def PopulateList(self, variable):
+        name_store = gtk.TreeStore(int,str)
+        for count, item in enumerate(self.data):
+            name_store.append(count, item)
+        comboview = self.builder.get_object("combobox1")
+        comboview.set_model(name_store)"""
+
 
 if __name__ == "__main__":
     gclass = WindowHandler()
